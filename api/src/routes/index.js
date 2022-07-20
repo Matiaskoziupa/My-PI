994f05d1627ee5b0ee1a6f6489ae9ed76cee1e8e
matiaskoziupa <matikoziupa@gmail.com>
2022-07-20 03:27:51 -0300

    back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -49,16 +49,10 @@ router.get("/videogames", async (req, res)=>{
     try{
         const apiInfo= await axios.get("https://api.rawg.io/api/games?key=e145758c71a14c72bd4afd6699d4d322");
         const apiData= apiInfo.data.results.map((s)=>{
-            const obj={
-            id:s.id,
+            const obj={    
             name:s.name,
-            description:s.description,
-            release_date:s.released,
-            rating:s.rating,
-            genres:s.genres.map(s=>s),
-            platforms:s.platforms.map(s=>s.platform.name),
+            genres:s.genres.map(s=>s.name),
             image:s.background_image,
-            createdInDb:s.createdInDb,
             }   
         return obj;
         })
@@ -68,6 +62,45 @@ router.get("/videogames", async (req, res)=>{
     } catch(error){
         res.status(404).json({error:error.message});
     }
+})
+router.post("/videogames", async(req, res)=>{
+    const {name, description, released, rating, platforms, background_image, genres}=req.body;
+    // if(!name || !description || !platforms){
+    //     res.status(400).json({msg:"Faltan datos"})
+    // }
+    if (!name || typeof name !== "string") {
+        return res.status(404).json({ error: "Invalid name" });
+    }
+    if (!description || typeof description !== "string") {
+        return res.status(404).json({ error: "Invalid description" });
+    }
+    if (!platforms || typeof platforms !== "string") {
+        return res.status(404).json({ error: "Invalid platforms" });
+    }
+    if (!genres || typeof genres !== "object") {
+        return res.json({ error: "Invalid genres" });
+    }
+    if (rating) {
+        if (typeof rating !== "number") {
+            return res.status(404).json({ error: "Invalid rating" });
+        }
+    }
+    if(released){
+        if(typeof released!=="string"){
+            return res.status(404).json({error:"Invalid released"})
+        }
+    }
+    try{
+        const obj={name, description,released, rating, platforms, background_image}
+        const newGame= await Videogame.create(obj)
+        newGame.addGenre(genres)
+        const aux=Videogame.findByPk(newGame.id, {
+            include: [{model: Genre}],
+    });
+    res.status(201).send(aux);
+} catch(error){
+    res.status(404).json({error: error.message})
+}
 })
 router.get("/genres", async(req, res)=>{
     try{
