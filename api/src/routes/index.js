a7cdf3a5dd47344fb96c71c3c17107d241c803eb
matiaskoziupa <matikoziupa@gmail.com>
2022-07-20 01:46:44 -0300

    commit

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,7 @@
 const axios= require("axios");
 const {Videogame, Genre}= require("../db")
 const { Router } = require('express');
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -9,6 +10,40 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+// const getApiInfo= async()=>{
+//     const apiInfo= await axios.get("https://api.rawg.io/api/games?key=e145758c71a14c72bd4afd6699d4d322");
+//     const apiData= apiInfo.data.results.map((s)=>{
+//             return {
+//             id:s.id,
+//             name:s.name,
+//             description:s.description,
+//             release_date:s.released,
+//             rating:s.rating,
+//             genres:s.genres.map(s=>s),
+//             platforms:s.platforms.map(s=>s.platform.name),
+//             image:s.background_image,
+//             createdInDb:s.createdInDb,
+//             }   
+//         })
+//         return apiData;
+// }
+// const getDbInfo= async ()=>{
+//     return await Videogame.findAll({
+//         include:{
+//             model:Genre,
+//             attributes:["name"],
+//             through:{
+//                 attributes: [],
+//             },
+//         }
+//     })
+// }
+// const getAllCharacters=async ()=>{
+//     const apiInfo= await getApiInfo();
+//     const dbInfo= await getDbInfo();
+//     const infoTotal= apiInfo.concat(dbInfo);
+//     return infoTotal;
+// }
 
 router.get("/videogames", async (req, res)=>{
     try{
@@ -20,8 +55,9 @@ router.get("/videogames", async (req, res)=>{
             description:s.description,
             release_date:s.released,
             rating:s.rating,
+            genres:s.genres.map(s=>s),
             platforms:s.platforms.map(s=>s.platform.name),
-            background_image:s.background_image,
+            image:s.background_image,
             createdInDb:s.createdInDb,
             }   
         return obj;
@@ -31,6 +67,22 @@ router.get("/videogames", async (req, res)=>{
         res.status(200).json(suma);
     } catch(error){
         res.status(404).json({error:error.message});
+    }
+})
+router.get("/genres", async(req, res)=>{
+    try{
+        const apiGenresUrl= await axios.get("https://api.rawg.io/api/genres?key=e145758c71a14c72bd4afd6699d4d322");
+        const apiGenresInfo= apiGenresUrl.data.results.map((s)=>{
+            const obj={
+                id:s.id,
+                name:s.name,
+            };
+        return obj;
+    })
+    Genre.bulkCreate(apiGenresInfo);
+    res.json({apiGenresInfo});
+    } catch(error){
+        res.status(404).json({error:error.message})
     }
 })
 
