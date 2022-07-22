b0b9a938be37850204abfb5123ce5b3fbf45fd24
matiaskoziupa <matikoziupa@gmail.com>
2022-07-22 06:09:45 -0300

    backend finished

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -10,6 +10,20 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+const getApiInfo= async()=>{
+    const apiInfo= await axios.get("https://api.rawg.io/api/games?key=e145758c71a14c72bd4afd6699d4d322");
+    const apiData= apiInfo.data.results.map((s)=>{
+        const obj={    
+            name:s.name,
+            genres:s.genres.map(s=>s.name),
+            image:s.background_image,
+        }   
+    return obj;
+    })
+}
+const getDbInfo= async()=>{
+
+}
 
 router.get("/videogames", async (req, res)=>{
     try{
@@ -88,7 +102,7 @@ router.get("/genres", (req, res)=>{
 //     res.status(404).json({error: error.message})
 // }
 // })
-router.post('/videogames', async (req, res) => {
+router.post("/videogames", async (req, res) => {
     const {background_image, name, release_date, genres, rating, description, platforms, createdInDb} = req.body;
     
         const newVideogame = await Videogame.create({
@@ -105,8 +119,84 @@ router.post('/videogames', async (req, res) => {
         })
         newVideogame.addGenre(genresDb)
         res.send("Videogame created succesfully")
-
 });
+router.get("/videogame/:id", async (req, res)=>{
+    const {id}=req.params;
+    const apiInfo= await axios.get("https://api.rawg.io/api/games?key=e145758c71a14c72bd4afd6699d4d322")
+    const apiRta=apiInfo.data.results.map((s)=>{
+        const obj={
+            name:s.name,
+            id:s.id,
+            genre:s.genres.map(s=>s.name),
+            description:s.description,
+            release_date:s.released,
+            rating:s.rating,
+            platforms:s.platforms.map(s=>s.platform.name),
+            image:s.background_image
+        }
+        return obj;
+    })
+    const getDbInfo= await Videogame.findAll({include:[{model:Genre}]});
+    const suma=[...apiRta,...getDbInfo];
+    if(id){
+        let videogameId= await suma.filter(s=> s.id==id)
+        videogameId.length?
+        res.status(200).json(videogameId) :
+        res.status(404).json("Not Found")
+    }
+})
 
+// router.get("/videogame/:id", async (req, res)=>{
+//     const { idVideogame } = req.params
+//     if (idVideogame.includes()) {
+//         let videogameDb = await Videogame.findOne({
+//             where: {
+//                 id: idVideogame,
+//             },
+//             include: Genre
+//         })
+//         videogameDb = JSON.stringify(videogameDb);
+//         videogameDb = JSON.parse(videogameDb);
+//         videogameDb.genres = videogameDb.genres.map(g => g.name);
+//         res.json(videogameDb)
+//     };
+
+//     try {
+//         const response = await axios.get("https://api.rawg.io/api/games?key=e145758c71a14c72bd4afd6699d4d322");
+//         let { name, background_image, genres, description, released: releaseDate, rating, platforms }
+//          = response.data;
+//         genres = genres.map(g => g.name);
+//         platforms = platforms.map(p => p.platform.name);
+//         return res.json({
+//             name,
+//             background_image,
+//             genres,
+//             description,
+//             releaseDate,
+//             rating,
+//             platforms
+//         })
+//     } catch (err) {
+//         return console.log(err)
+//     }
+// })
+router.get("/games/:name", async(req, res)=>{
+    const {name}=req.params;
+    const apiInfo= await axios.get("https://api.rawg.io/api/games?key=e145758c71a14c72bd4afd6699d4d322")
+    const apiRta=apiInfo.data.results.map((s)=>{
+        const obj={
+            name:s.name,
+        }
+        return obj;
+    })
+    const getDbInfo= await Videogame.findAll({include:[{model:Genre}]});
+    const suma=[...apiRta,...getDbInfo];
+    if(name){
+        let nameVideogame= await suma.filter(s=> s.name.toLowerCase().includes(name.toLowerCase()))
+        nameVideogame.length?
+        res.status(200).json(nameVideogame) :
+        res.status(404).json("Not Found")
+    }
+})
 
 module.exports = router;
